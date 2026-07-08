4a01be1951e2fdf6f66f3d8813d772893712e89c
Hiya Bhakta <hiyabhakta455@gmail.com>
2026-07-08 22:16:42 +0530

    Time: 1 ms (98.55%), Space: 47.8 MB (25.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,19 +5,20 @@ class Solution {
 
         boolean firstRow = false;
         boolean firstCol = false;
-
+        
+        //check in row
         for(int j = 0; j < column; j++) {
             if(matrix[0][j] == 0) {
                 firstRow = true;
             }
         }
-
+        //check in column
         for(int i = 0; i < row; i++) {
             if(matrix[i][0] == 0) {
                 firstCol = true;
             }
         }
-
+        //mark in row and column
         for(int i = 1; i < row; i++) {
             for(int j = 1; j < column; j++) {
                 if(matrix[i][j] == 0) {
@@ -26,7 +27,7 @@ class Solution {
                 }
             }
         }
-
+        //set to zero
         for(int i = 1; i < row; i++) {
             for(int j = 1; j < column; j++) {
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -34,13 +35,13 @@ class Solution {
                 }
             }
         }
-
+        //mark in row
         if(firstRow) {
             for(int j = 0; j < column; j++) {
                 matrix[0][j] = 0;
             }
         }
-
+        //mark in column
         if(firstCol) {
             for(int i = 0; i < row; i++) {
                 matrix[i][0] = 0;
