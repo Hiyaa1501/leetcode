3e449c516f00aedbf74f7166e0fc7f6b29dba841
Hiya Bhakta <hiyabhakta455@gmail.com>
2026-09-06 23:18:04 +0530

    Time: 1 ms (99.63%), Space: 44.5 MB (100%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.java
+++ 2951-find-the-peaks/2951-find-the-peaks.java
@@ -2,7 +2,7 @@ class Solution {
     public List<Integer> findPeaks(int[] mountain) {
         List<Integer> peak = new ArrayList<>();
 
-        for(int i = 1; i < mountain.length - 1; i++) {
+        for(int i = 1; i < mountain.length - 1; i++) { //first and last cannot be peak
             if(mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) {
                 peak.add(i);
             }
